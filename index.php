48fc927777c049a614eec812f75cd5c77fe8cd38
harviacode <hariprasetyo0212@gmail.com>
2014-12-02 20:18:43 +0700

    template

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php include 'header.php'; ?>
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <?php include 'menu.php'; ?>
+        <div id="page-wrapper"><?php include 'loader.php'; ?></div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+<?php include 'footer.php'; ?>
