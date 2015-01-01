e677373d1936b1bdda85923cfcfa9b536b2d0ca7
harviacode <hariprasetyo0212@gmail.com>
2015-01-01 13:14:29 +0700

    cek terakhir

--- login.php
+++ login.php
@@ -49,6 +49,8 @@ if (isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+    
+    <link rel="icon" type="image/png" href="img/favicon.jpg">
 
     <title>Login Aplikasi Gaji</title>
 
